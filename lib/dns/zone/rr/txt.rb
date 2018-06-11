09f247c2346e55b30055e8785f89db0b30d130c6
Rihards <krihards@me.com>
2018-06-11 18:58:19 +0300

    split long txt records in multiple lines

--- lib/dns/zone/rr/txt.rb
+++ lib/dns/zone/rr/txt.rb
@@ -7,7 +7,15 @@ class DNS::Zone::RR::TXT < DNS::Zone::RR::Record
 
   def dump
     parts = general_prefix
-    parts << %Q{"#{text}"}
+    chunks = chunk(text, 95)
+    txt_value = ''
+    size = chunks.size
+    chunks.each_with_index do |chunk, index|
+      txt_value << "\t\t\t" unless index == 0
+      txt_value << %Q{"#{chunk}"}
+      txt_value << "\n" unless index == size - 1
+    end
+    parts << "(#{txt_value})"
     parts.join(' ')
   end
 
@@ -17,6 +25,10 @@ class DNS::Zone::RR::TXT < DNS::Zone::RR::Record
     # extract text from within quotes; allow multiple quoted strings; ignore escaped quotes
     @text = rdata.scan(/"#{DNS::Zone::RR::REGEX_STRING}"/).join
     self
+  end
+
+  def chunk(string, size)
+    string.scan(/.{1,#{size}}/)
   end
 
 end
--- test/rr/txt_test.rb
+++ test/rr/txt_test.rb
@@ -18,6 +18,19 @@ class RR_TXT_Test < DNS::Zone::TestCase
     assert_equal 'labelname 2w IN TXT "test text"', rr.dump
   end
 
+  def test_build_rr__txt_multiline
+    rr = DNS::Zone::RR::TXT.new
+
+    # ensure we can set text parameter
+    rr.text = 'k=rsa; p=cIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3gOGfa9UXUwN1f5movOSQZ5lUqa2ZkCgSNR4UqSrp6Hysr3B7yJk77Zhd3tJ3GS0padpuQS33EeJfRMNBS+YyNXfTEFHg02+Gs4JH4ZWaZMNQNXeZrPWqVwbzaRjAXw/z18utbiDf9EUoprsJNI6zkj92kdYcta+2Pcpp2qyCUJbXJ2VL1akAcBy1lAfU+s6JdH7uqwOVwgtocVDdqraf00SZ4LjYDXzYw/7oW0+zgzDpFroT+F0wtakN+gx1yovRLETeuuBPBpH9drrNo1r5B6TziUY+l/Roca1kPb7tK75Oa04aU0hvO75+G7mE+XGj0oWHmFTUZA+uZ9GkaGpfwIDAQAB;'
+    puts rr.dump
+    assert_equal '@ IN TXT ("k=rsa; p=cIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3gOGfa9UXUwN1f5movOSQZ5lUqa2ZkCgSNR4UqSrp6"
+			"Hysr3B7yJk77Zhd3tJ3GS0padpuQS33EeJfRMNBS+YyNXfTEFHg02+Gs4JH4ZWaZMNQNXeZrPWqVwbzaRjAXw/z18utbiDf"
+			"9EUoprsJNI6zkj92kdYcta+2Pcpp2qyCUJbXJ2VL1akAcBy1lAfU+s6JdH7uqwOVwgtocVDdqraf00SZ4LjYDXzYw/7oW0+"
+			"zgzDpFroT+F0wtakN+gx1yovRLETeuuBPBpH9drrNo1r5B6TziUY+l/Roca1kPb7tK75Oa04aU0hvO75+G7mE+XGj0oWHmF"
+			"TUZA+uZ9GkaGpfwIDAQAB;")', rr.dump
+  end
+
   def test_load_rr__txt
     rr = DNS::Zone::RR::TXT.new.load('txtrecord IN TXT "test text"')
     assert_equal 'txtrecord', rr.label
